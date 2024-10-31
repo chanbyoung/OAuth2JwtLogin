7ec187ae069257b761f42aaf002628a8ccaaebf7
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-01 01:37:17 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/loginStudy/oauth2andJwt/Oauth2andJwtApplication.java
@@ -0,0 +1,13 @@
+package com.loginStudy.oauth2andJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2andJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2andJwtApplication.class, args);
+	}
+
+}
