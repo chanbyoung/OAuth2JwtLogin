f131926cc57e38095901db6f89e9ba36862c4a50
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-01 20:25:44 +0900

    feat: 회원의 엔티티 및 DAO 생성

---
+++ src/main/java/com/loginStudy/oauth2andJwt/domain/member/entity/Member.java
@@ -0,0 +1,40 @@
+package com.loginStudy.oauth2andJwt.domain.member.entity;
+
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Enumerated;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "member")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id", nullable = false)
+    private Long id;
+
+    // 계정의 아이디
+    @Column(name = "account", nullable = false)
+    private String account;
+
+    // 비밀 번호
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    //권한
+    @Enumerated(STRING)
+    @Column(name = "role", nullable = false)
+    private Role role;
+}
---
+++ src/main/java/com/loginStudy/oauth2andJwt/domain/member/entity/Role.java
@@ -0,0 +1,9 @@
+package com.loginStudy.oauth2andJwt.domain.member.entity;
+
+/**
+ * 사용자 권한
+ */
+public enum Role {
+    USER,
+    ADMIN
+}
