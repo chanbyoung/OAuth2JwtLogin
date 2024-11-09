be53ad3e68c89ce1edfe368b529bcd88a3b1b8d7
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-09 13:24:07 +0900

    feat: 소셜 로그인 성공시 동작하는 핸들러 구현

--- src/main/java/com/loginStudy/oauth2andJwt/global/dto/response/AuthResponseDto.java
+++ src/main/java/com/loginStudy/oauth2andJwt/global/dto/response/AuthResponseDto.java
@@ -1,5 +1,6 @@
 package com.loginStudy.oauth2andJwt.global.dto.response;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,5 +13,12 @@ import lombok.NoArgsConstructor;
 public class AuthResponseDto {
     private String accessToken;
     private String refreshToken;
-
+    public String toJson() {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.writeValueAsString(this);
+        } catch (Exception e) {
+            throw new RuntimeException("JSON 변환 중 오류 발생", e);
+        }
+    }
 }
