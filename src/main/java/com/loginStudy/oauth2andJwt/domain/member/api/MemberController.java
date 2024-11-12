e9740e688d4de6c35a7891d63fab8744757dc8c3
pcb7893@naver.com <122460524+chanbyoung@users.noreply.github.com>
2024-11-13 03:16:57 +0900

    feat: 프로필 조회 엔드포인트 추가

--- src/main/java/com/loginStudy/oauth2andJwt/domain/member/api/MemberController.java
+++ src/main/java/com/loginStudy/oauth2andJwt/domain/member/api/MemberController.java
@@ -1,13 +1,17 @@
 package com.loginStudy.oauth2andJwt.domain.member.api;
 
 import com.loginStudy.oauth2andJwt.domain.member.application.MemberService;
+import com.loginStudy.oauth2andJwt.domain.member.dto.rep.MemberProfileRepDto;
 import com.loginStudy.oauth2andJwt.domain.member.dto.req.MemberAdditionalSetupReqDto;
+import com.loginStudy.oauth2andJwt.global.auth.application.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/member")
 @RequiredArgsConstructor
@@ -23,5 +27,11 @@ public class MemberController {
     ) throws IOException {
         memberService.setupProfile(account, setupDto);
         return ResponseEntity.ok("추가 설정이 완료되었습니다.");
+    }
+    @GetMapping("/profile")
+    public ResponseEntity<MemberProfileRepDto> getMemberProfile(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        String memberAccount = userDetails.getUsername();
+        MemberProfileRepDto profile = memberService.getMemberProfile(memberAccount);
+        return ResponseEntity.ok(profile);
     }
 }
